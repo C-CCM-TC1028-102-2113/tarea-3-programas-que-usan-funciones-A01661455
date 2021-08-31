cb88d2db9fb77b71670056483436e203815a305f
A01661455 <89362416+A01661455@users.noreply.github.com>
2021-08-31 10:02:50 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,23 +1,25 @@
-#Volúmen de un prisma rectangular 
-# A01661455 Carlos Peña Gutiérrez 
-
 def main():
   #escribe tu código abajo de esta línea
+  #Carlos Peña Gutiérrez 
+  #A01661455
 
  def ba(base,altura):
-  area=base*altura
-  return area
+    area=base*altura
+    return area
 
- def volumen(area,prof,vol):
-  vol=area*prof
-  return vol, area
+ def volum(prof,area):
+    v= area * prof
+    return v
+
 
 base=float(input("Dame la base: "))
 altura=float(input("Dame la altura: "))
 prof=float(input("Dame la profundidad: "))
 
-salida= (vol)
-print(salida)
+volum= (base*altura*prof)
+salida= (volum)
+print("El volúmen del prisma es: ",salida)
 
 if __name__ == '__main__':
     main()
+
