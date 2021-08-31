4e32828f58c41ed69871e565d7bb078bdd7f2285
A01661455 <89362416+A01661455@users.noreply.github.com>
2021-08-31 10:40:48 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,22 @@
+#Carlos Peña Gutiérrez 
+#A01661455
+import math 
+
+year=float(input)
+
+def bisiesto(year):
+     if (year%4==0):
+      year=True
+     else:
+      year=False
+     return year
+
+ex=year
+print (ex)
 
 def main():
     #escribe tu código abajo de esta línea
+   
     pass
 
 if __name__=='__main__':
